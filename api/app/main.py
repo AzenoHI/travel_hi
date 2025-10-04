2a0bda986e33a895faf7c2d1ff5c80250ebfdeb3
Kinetics20 <kinetics20@gmail.com>
2025-10-04 23:05:53 +0200

    fixed fastapi

--- api/app/main.py
+++ api/app/main.py
@@ -1,18 +1,11 @@
-from fastapi import Depends, FastAPI
+from app.routers.v1.api import api_router
+from fastapi import FastAPI
 
-from dotenv import load_dotenv
-from app.routers import disruptions
-from sqlalchemy.orm import Session
-from .db.database import Base, engine, get_session
-from .routers.v1.api import api_router
-load_dotenv()
 
 app = FastAPI(
     title="Travel Hi API", version="1.0.0", description="API for managing travels."
 )
 
-
-app.include_router(disruptions.router)
 app.include_router(api_router, prefix="/api/v1")
 
 
--- api/app/routers/v1/api.py
+++ api/app/routers/v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.routers.v1.endpoints import event
+from app.routers.v1.endpoints import event, disruptions
 from app.routers.v1.endpoints import report
 
 api_router = APIRouter()
 
 api_router.include_router(event.router, prefix="/events", tags=["Events"])
 api_router.include_router(report.router, prefix="", tags=["reports"])
+api_router.include_router(disruptions.router, prefix="/disruptions", tags=["disruptions"])
