5726e4bae7a58d75c298779b340d3e84857362a2
Paweł Konior <pawel.konior@gmail.com>
2025-10-05 10:49:56 +0200

    fix

--- app/src/pages/Incidents.tsx
+++ app/src/pages/Incidents.tsx
@@ -7,9 +7,6 @@ export default function Incidents() {
   return (
     <Container maxWidth="md" sx={{ py: 3 }}>
       <Stack spacing={2}>
-        <Typography variant="h5" fontWeight={700}>
-          TravelHI — Incydenty
-        </Typography>
 
         <ReportIncidentForm />
 
